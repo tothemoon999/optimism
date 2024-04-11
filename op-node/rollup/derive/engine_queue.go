ba91b0c27758dbe5bc1f96f1f262385000949cd4
SeniorDeveloper64 <seniorsoftdeveloper64@gmail.com>
2024-04-12 05:20:20 +0900

    sdsdf

--- op-node/rollup/derive/engine_queue.go
+++ op-node/rollup/derive/engine_queue.go
@@ -497,7 +497,7 @@ func (eq *EngineQueue) tryNextUnsafePayload(ctx context.Context) error {
 	}
 	eq.unsafePayloads.Pop()
 	eq.log.Trace("Executed unsafe payload", "hash", ref.Hash, "number", ref.Number, "timestamp", ref.Time, "l1Origin", ref.L1Origin)
-	eq.logSyncProgress("unsafe payload from sequencer")
+	eq.logSyncProgress("unsafe payload from sequencer!!!")
 
 	attrs := eq.safeAttributes.attributes
 	eq.log.Trace("Pending Transactions: ", attrs.Transactions)
